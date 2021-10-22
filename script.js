eb7b9b3b47c1acc70dac9d8e6ed39e9e193abb2c
Kelsey Cummings <kelseycummings923@gmail.com>
2021-10-22 09:32:07 -0500

    Spread Destructing Exercise

--- script.js
+++ script.js
@@ -1,95 +1,82 @@
-// Spread and Destructuring Exercise
+// SPREAD AND DESTRUCTURING WITH FUNCTIONS EXERCISE
 
 // 1a
-const mcuShows = ["Loki", "What If..."];
-
-// 1b
-const starWarsShows = ["The Mandalorian", "The Bad Batch"];
-
-// 1c
-const disneyPlusShows = [...mcuShows, ...starWarsShows, "Monsters At Work"];
-
-// 1d
-console.log(disneyPlusShows);
-
-// 2a
-const netflixMovies = {
-    action: "Extraction",
-    crime: "The Irishman"
-};
-
-// 2b
-const amazonPrimeMovies = {
-    action: "The Tomorrow War",
-    drama: "One Night In Miami"
-};
-
-// 2c
-const streamingMovies = {
-    ...amazonPrimeMovies,
-    ...netflixMovies,
-    musical: "Hamilton"
-}
-
-// 2d 
-console.log(streamingMovies);
-
-// 3a 
-const disneyJunior = [
-    "Mickey Mouse Clubhouse",
-    "Spidey and His Amazing Friends"
-]; 
-
-// 3b
-const [mickey, spidey] = disneyJunior;
-
-// 3c 
-console.log(mickey, spidey);
-// console.log(disneyJunior);
-
-// 4a
-const avengers = {
-    warMachine: "James Rhodes",
-    theHulk: "Bruce Banner"
-};
-
-// 4b
-const {warMachine, theHulk} = avengers;
-
-// 4c
-console.log(warMachine, theHulk);
-
-// 4d
-const moreAvengers = {
-    blackWidow: "Natasha Romanoff",
-    hawkeye: "Clint Barton",
-    ironMan: "Tony Stark"
-};
-
-// 4e
-const {blackWidow: nat, ...others} = moreAvengers;
-
-// 4f
-console.log(nat, others);
-
-// BONUS
-const bonus = {
-    first: [1, 2, 3],
-    second: [4, 5, 6],
-    third: [7, 8, 9]
-  };
-
-  // 5a
-  const all = [
-      ...bonus["first"],
-      ...bonus["second"],
-      ...bonus["third"]
+const jPDinos = [
+    "Velociraptors",
+    "Tyrannosaurus Rex",
+    "Dilophosaurus"
   ];
-  console.log(all);
-
-  // 5b
-  const {first, second, third} = bonus;
-  const [one, two, three] = first;
-  const [four, five, six] = second;
-  const [seven, eight, nine] = third;
-  console.log(bonus); 
+  
+  // 1b
+  function seeDinos(a, ...b){
+    console.log(a);
+    console.log(b);
+  }
+  seeDinos(...jPDinos);
+  /*
+  Velociraptors
+  [ 'Tyrannosaurus Rex', 'Dilophosaurus' ]
+  */
+  
+  console.log("============");
+  
+  // 2a
+  const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+  };
+  
+  // 2b
+  seeCharacters = ({ellieSattler}) => {
+    console.log(ellieSattler);
+  }
+  seeCharacters(jPCharacters); // Laura Dern
+  
+  console.log("==========");
+  
+  // BONUS
+  // 3a
+  const jurassicParkMovies = [
+    {
+      one: "Jurassic Park",
+      two: "The Lost World: Jurassic Park",
+      three: "Jurassic Park III"
+    },
+    {
+      four: "Jurassic World",
+      five: "Jurassic World: Fallen Kingdom",
+      six: "Jurassic World: Dominion"
+    }
+  ];
+  
+  // 3b
+  seeJPMovies = ([{one, two, three}, {four, five, six}]) => {
+    console.log(one);
+   console.log(two);
+   console.log(three);
+   console.log(four);
+   console.log(five);
+   console.log(six);
+  }
+  seeJPMovies(jurassicParkMovies);
+  /*
+  Jurassic Park
+  The Lost World: Jurassic Park
+  Jurassic Park III
+  Jurassic World
+  Jurassic World: Fallen Kingdom
+  Jurassic World: Dominion
+  */
+  
+  // OR
+  
+  // seeJPMovies = ({one, two, three}, {four, five, six}) => {
+  //   console.log(one);
+  //  console.log(two);
+  //  console.log(three);
+  //  console.log(four);
+  //  console.log(five);
+  //  console.log(six);
+  // }
+  // seeJPMovies(...jurassicParkMovies);
